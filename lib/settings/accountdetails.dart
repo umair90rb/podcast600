b2b194ee5b5fe1b242cc14199bbd45b618d8a854
Umair <umair90rb@gmail.com>
2021-02-22 21:09:42 +0500

    comment buddy link

--- lib/settings/accountdetails.dart
+++ lib/settings/accountdetails.dart
@@ -301,59 +301,59 @@ class _AccountdetailsState extends State<Accountdetails> {
                       ),
                     ),
                   ),
-                  SizedBox(height: 20),
-                  Container(
-                    child: Padding(
-                      padding: EdgeInsets.only(left: 10),
-                      child: Row(
-                        children: <Widget>[
-                          Expanded(
-                            child: Text(
-                              'My buddy link',
-                              style: TextStyle(
-                                  color: Colors.blueGrey, fontSize: 13),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 5,
-                  ),
-                  Padding(
-                    padding: EdgeInsets.only(),
-                    child: Card(
-                      elevation: 0,
-                      child: ListTile(
-                        selectedTileColor: Colors.white,
-                        tileColor: Colors.white,
-                        title: Text(
-                          'My buddy link',
-                          style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.black54,
-                          ),
-                          // textScaleFactor: 1.5,
-                        ),
-                        trailing: Text(
-                          "CHANGE",
-                          style: TextStyle(
-                              color: Colors.orange,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 16),
-                        ),
-                        onTap: () {
-                          // Navigator.push(
-                          //   context,
-                          //   MaterialPageRoute(
-                          //       builder: (context) => SettingProfile()),
-                          // );
-                        },
-                      ),
-                    ),
-                  ),
+                  // SizedBox(height: 20),
+                  // Container(
+                  //   child: Padding(
+                  //     padding: EdgeInsets.only(left: 10),
+                  //     child: Row(
+                  //       children: <Widget>[
+                  //         Expanded(
+                  //           child: Text(
+                  //             'My buddy link',
+                  //             style: TextStyle(
+                  //                 color: Colors.blueGrey, fontSize: 13),
+                  //           ),
+                  //         ),
+                  //       ],
+                  //     ),
+                  //   ),
+                  // ),
+                  // SizedBox(
+                  //   height: 5,
+                  // ),
+                  // Padding(
+                  //   padding: EdgeInsets.only(),
+                  //   child: Card(
+                  //     elevation: 0,
+                  //     child: ListTile(
+                  //       selectedTileColor: Colors.white,
+                  //       tileColor: Colors.white,
+                  //       title: Text(
+                  //         'My buddy link',
+                  //         style: TextStyle(
+                  //           fontSize: 18,
+                  //           fontWeight: FontWeight.bold,
+                  //           color: Colors.black54,
+                  //         ),
+                  //         // textScaleFactor: 1.5,
+                  //       ),
+                  //       trailing: Text(
+                  //         "CHANGE",
+                  //         style: TextStyle(
+                  //             color: Colors.orange,
+                  //             fontWeight: FontWeight.bold,
+                  //             fontSize: 16),
+                  //       ),
+                  //       onTap: () {
+                  //         // Navigator.push(
+                  //         //   context,
+                  //         //   MaterialPageRoute(
+                  //         //       builder: (context) => SettingProfile()),
+                  //         // );
+                  //       },
+                  //     ),
+                  //   ),
+                  // ),
                   SizedBox(height: 5,),
                   Padding(
                     padding: EdgeInsets.only(),
